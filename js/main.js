b6e89b7da77b3a543202c44f2af563c06d671e40
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-08-09 23:29:52 +0300

    donation_page

--- js/main.js
+++ js/main.js
@@ -325,3 +325,28 @@
 $(document).ready(function() {
     $(".owl-carousel").owlCarousel();
 });
+
+
+/*****************Payment Method*************/
+
+$(document).ready(function() {
+
+    $('.method').on('click', function() {
+        $('.method').removeClass('blue-border');
+        $(this).addClass('blue-border');
+    });
+
+})
+var $cardInput = $('.input-fields input');
+
+$('.next-btn').on('click', function(e) {
+
+    $cardInput.removeClass('warning');
+
+    $cardInput.each(function() {
+        var $this = $(this);
+        if (!$this.val()) {
+            $this.addClass('warning');
+        }
+    })
+});
